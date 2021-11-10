0d2db8e5bb8b97b5be2841f9ed36a301267f641b
HoangNamBN <nguyenvannamtgdd35@gmail.com>
2021-11-10 22:49:52 +0700

    Update

---
+++ LapTrinhTriAnh/src/CauHoiPhongVan/CovertString2Int.java
@@ -0,0 +1,9 @@
+package CauHoiPhongVan;
+
+public class CovertString2Int {
+    public static void main(String[] args) {
+        String a = "123";
+        int b = Integer.valueOf(a);
+        System.out.println(b);
+    }
+}
