bc013ff474084ae314c70f6fab98e7d30f95d25d
HoangNamBN <nguyenvannamtgdd35@gmail.com>
2021-11-30 07:33:57 +0700

    update

---
+++ LapTrinhTriAnh/src/T3H/javacore/JavaTutorial/JavaVariables/Exericise1.java
@@ -0,0 +1,8 @@
+package T3H.javacore.JavaTutorial.JavaVariables;
+
+public class Exericise1 {
+    public static void main(String[] args) {
+        String carName = "Volvo";
+        System.out.println(carName);
+    }
+}
